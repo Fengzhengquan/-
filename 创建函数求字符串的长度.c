eea7cbce2b2eceb037d2eae73937fb26d19500f2
冯正权 <837927034@qq.com>
2021-02-27 11:57:05 +0800

    创建函数使用函数递归，不创建临时变量，求字符串长度

---
+++ 创建函数求字符串的长度.c
@@ -0,0 +1,37 @@
+﻿//1、使用循环语句：
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+
+int main()
+{
+	char arr[] = "fzq";
+	printf("字符串长度为%d", my_strlen(arr));
+	return 0;
+}
+
+//2、使用函数递归，不创造临时变量；
+int my_strlen(char* str)
+{
+	if (*str != '\0')
+	{
+		str++;
+		return 1 + my_strlen(str);
+	}
+	else
+		return 0;
+}
+
+int main()
+{
+	char arr[] = "fzq";
+	printf("字符串长度为%d", my_strlen(arr));
+	return 0;
+}
